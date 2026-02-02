bc0452f045b8f4ff39236574856c1db937967daf
janangindia82 <janangindia82@gmail.com>
2026-02-02 15:31:02 +0530

    updated to password

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,17 +64,15 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = azurerm_resource_group.rg.location
   size                = var.vm_size
   admin_username      = "azureuser"
+  admin_password      = "Namashiva@1982" # In a real project, we would hide this!
+  disable_password_authentication = false
   network_interface_ids = [azurerm_network_interface.nic.id]
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  os_disk { 
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS" 
-    }
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
